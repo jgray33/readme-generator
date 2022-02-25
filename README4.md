2fa9ab5acafb4f4c4ccba1fee472cfc9a52b6577
Julia <julia.gray30@gmail.com>
2022-02-25 21:10:08 +0000

    license link and license badge functions added, readMe generating

---
+++ README4.md
@@ -0,0 +1,48 @@
+
+undefined
+
+# ReadMe generator 
+
+## Description
+It's a generator that generates a readme 
+
+
+
+### Table of contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributors](#contributors)
+* [Tests](#tests)
+* [Questions](#questions)
+
+<a name="installation"></a>
+#### Installation 
+run and play!
+
+
+<a name="usage"></a>
+### Usage 
+Install 
+
+
+<a name="license"></a>
+### License 
+
+This application is covered under the [GPLv2](undefined) license.  
+
+
+<a name="contributors"></a>
+### Contributors 
+Julia
+
+<a name="tests"></a>
+### Tests 
+test
+
+<a name="questions"></a>
+#### Questions
+
+If you have any questions, visit my [GitHub profile](www.github.com/jgray33) or email me: julia-gray33@hotmail.com 
+
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-let inquirer = require('inquirer');
-const fs = require("fs")
+let inquirer = require("inquirer");
+const fs = require("fs");
 
-const generateReadme = ({title, description, installation, usage, license, contributors, testInstructions, gitHub, email, licenseBadge}) =>
-`
+const generateReadme = ({
+  title,
+  description,
+  installation,
+  usage,
+  license,
+  contributors,
+  testInstructions,
+  gitHub,
+  email,
+  licenseBadge,
+  licenseLink
+}) =>
+  `
+${licenseBadge}
+
 # ${title} 
 
 ## Description
 ${description} 
 
-${licenseBadge}
+
 
 ### Table of contents 
 
@@ -25,139 +39,157 @@ ${installation}
 
 
 <a name="usage"></a>
-#### Usage 
+### Usage 
 ${usage}
 
 
 <a name="license"></a>
-#### License 
-${license}
+### License 
+
+This application is covered under the [${license}](${licenseLink}) license.  
 
 
 <a name="contributors"></a>
-#### Contributors 
+### Contributors 
 ${contributors}
 
 <a name="tests"></a>
-#### Tests 
+### Tests 
 ${testInstructions}
 
 <a name="questions"></a>
-##### Questions
+#### Questions
 
 If you have any questions, visit my [GitHub profile](www.github.com/${gitHub}) or email me: ${email} 
 
+`;
 
-`
-
-
-
-inquirer
-  .prompt([ 
+inquirer. prompt([
       {
-    type:"input",
-    name:"title",
-    message:"What is the title of your project?",
-      },
-      {
-    type:"input",
-    name:"description",
-    message:"How would you describe your project?",
-      },
-      {
-    type:"input",
-    name:"installation",
-    message:"Describe how you would install your project?",
-      },
-      {
-    type:"input",
-    name:"usage",
-    message:"How do you use the project?",
-      },
-      {
-    type:"list",
-    name:"license",
-    message:"what about a license?",
-    choices:["MIT","GPLv2","Apache","GPLv3","BSD 3-clause","Unlicense"]
-      },
-      {
-    type:"input",
-    name:"contributors",
-    message:"Who are the other contributors to this project?",
-      },
-      {
-    type:"input",
-    name:"testInstructions",
-    message:"What are the test instructions?",
-      },
-      {
-    type:"input",
-    name:"gitHub",
-    message:"What is your GitHub user name?",
-      },
-      {
-    type:"input",
-    name:"email",
-    message:"What is your email address?",
-      },
+      type: "input",
+      name: "userName",
+      message: "So who is the proud owner of this project? Enter your name",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "How would you describe your project?",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Describe how you would install your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How do you use the project?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "what about a license?",
+      choices: ["MIT", "GPLv2", "Apache", "GPLv3", "BSD 3-clause", "Unlicense"],
+    },
+    {
+      type: "input",
+      name: "contributors",
+      message: "Who are the other contributors to this project?",
+    },
+    {
+      type: "input",
+      name: "testInstructions",
+      message: "What are the test instructions?",
+    },
+    {
+      type: "input",
+      name: "gitHub",
+      message: "What is your GitHub user name?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email address?",
+    },
   ])
-  .then((answers) => {
-         let readMePageContent = generateReadme(answers)
-    fs.writeFile("README2.md", readMePageContent, (err) => 
-    err ? console.log(err): console.log("Read me generated"))
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
 
-  function renderLicenseBadge() {
-    switch(answers.license) {
-        case "MIT":
-            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-            break;
-        case "GPLv2":
-            licenseBadge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
-            break;
-        case "Apache":
-            licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-            break
-        case "GPLv3":
-            licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
-            break
-        case "BSD 3-clause":
-            licenseBadge = "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
-            break
-        case "Unlicense":
-            licenseBadge = ""
-                }
+     .then((answers) => {
+      console.log(answers)
+      renderLicenseBadge(answers.license)
+      console.log(licenseBadge)
+      renderLicenseLink(answers.license)
+      let readMePageContent = generateReadme(answers)
+      fs.writeFile("README4.md", readMePageContent, (err) =>
+        err ? console.log(err) : console.log("Read me generated"))
+     })
+ 
+
+  
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+     return licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "GPLv2":
+       return licenseBadge = "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)";
+      break;
+    case "Apache":
+      return licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "GPLv3":
+     return licenseBadge =  "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "BSD 3-clause":
+      return licenseBadge =  "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      break;
+    case "Unlicense":
+      return licenseBadge = licenseBadge = "";
+  }
+  }
+
+function renderLicenseLink(license) {
+switch(license) {
+  case "MIT":
+    return licenseLink = "https://opensource.org/licenses/MIT";
+     break;
+   case "GPLv2":
+     return licenseLink = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"
+     break;
+   case "Apache":
+     return licenseLink = "https://opensource.org/licenses/Apache-2.0";
+     break;
+   case "GPLv3":
+    return licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
+     break;
+   case "BSD 3-clause":
+     return licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+     break;
+   case "Unlicense":
+     return licenseLink = licenseBadge = "";
 }
-
-// function renderLicenseLink(license) {} {
-
-// }
+} 
 
 // function renderLicenseSection(license) {
 
 // }
 
-
-  // TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
 
 // // TODO: Create a function that returns the license link
 // // If there is no license, return an empty string
-// 
+//
 
 // // TODO: Create a function that returns the license section of README
 // // If there is no license, return an empty string
 // function renderLicenseSection(license) {}
 
-// // TODO: Create a function to generate markdown for README
+// // TODO: Create a 
 // function generateMarkdown(data) {
 //   return `# ${data.title}
 
@@ -165,3 +197,4 @@ inquirer
 // }
 
 // module.exports = generateMarkdown;
+// module.exports 
